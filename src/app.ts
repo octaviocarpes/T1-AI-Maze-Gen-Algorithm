d3a95c7bb85ce95d74634206b5a824afb601896c
Octavio Carpes <octavio.carpes10@gmail.com>
2020-10-15 11:22:37 -0300

    Atualizaod app

--- src/app.ts
+++ src/app.ts
@@ -2,38 +2,25 @@ import { Labirinto as Labirinto } from "./labirinto";
 import { GeneticAlgorithm as AlgoritmoGenetico } from "./algortimoGenetico";
 import { Population as Populacao } from "./populacao";
 import { Cromossomo } from "./cromossomo";
-import { Grafo } from './grafo'
-import { procuraAestrela } from './a-star'
 
-/** Inicializacao de todo o algoritmo e estrutura */
-const numeroMaximoGeracoes: number = 1000;
+const numeroMaximoGeracoes: number = 1000
+;
 const labirinto = new Labirinto();
-const algoritmoGenetico = new AlgoritmoGenetico(200, 0.05, 0.9, 2, 10);
-let populacao: Populacao = algoritmoGenetico.iniciarPopulacao(128);
 
-const grafo = new Grafo()
-
-procuraAestrela(grafo, [0,0], [11,11])
+const algoritmoGenetico = new AlgoritmoGenetico(200, 0.05, 0.9, 2, 25);
+let populacao: Populacao = algoritmoGenetico.iniciarPopulacao(130);
 
 algoritmoGenetico.avaliacao(labirinto, populacao)
 
 let geracao : number = 1;
 
-/**  Conforme conversado com a professora, temos mias uma condição de parada que verifica se o score atingiu o valor ideal (A definir ainda) */
-// while(algoritmoGenetico.condicaoDeTerminioAtendida(geracao, numeroMaximoGeracoes) === false) {
-//     let cromossomo : Cromossomo = populacao.getCromossomoComMelhorAptidao(0);
+while(algoritmoGenetico.condicaoDeTerminioAtendida(geracao, numeroMaximoGeracoes) === false) {
 
-//     populacao = algoritmoGenetico.cruzemento(populacao)
-//     populacao = algoritmoGenetico.mutacao(populacao)
+    populacao = algoritmoGenetico.cruzemento(populacao)
+    populacao = algoritmoGenetico.mutacao(populacao)
+    algoritmoGenetico.avaliacao(labirinto, populacao)
+    geracao++;
+}
 
-//     algoritmoGenetico.avaliacao(labirinto, populacao )
-
-//     geracao++;
-
-// }
-
-// let cromossomo : Cromossomo = populacao.getCromossomoComMelhorAptidao(0);
-// console.log(cromossomo.getCromossomo())
-
-// let x : number = 0
-// let y : number = 0
+let cromossomo : Cromossomo = populacao.getCromossoMaisApto(0);
+console.log(cromossomo.getCromossomo())
