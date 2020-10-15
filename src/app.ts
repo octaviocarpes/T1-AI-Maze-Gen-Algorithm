767ac23c299b45f4309a8a5410ffc7a8a433fbeb
Octavio Carpes <octavio.carpes10@gmail.com>
2020-10-15 17:36:09 -0300

    Finished app

--- src/app.ts
+++ src/app.ts
@@ -13,21 +13,29 @@ let populacao: Populacao = algoritmoGenetico.iniciarPopulacao(200);
 
 algoritmoGenetico.avaliacao(labirinto, populacao)
 
-let geracao : number = 1;
+let geracao: number = 1;
 
-while(algoritmoGenetico.condicaoDeTerminioAtendida(geracao, numeroMaximoGeracoes) === false) {
-    populacao = algoritmoGenetico.cruzemento(populacao)
-    populacao = algoritmoGenetico.mutacao(populacao)
-    algoritmoGenetico.avaliacao(labirinto, populacao)
-    geracao++;
+while (algoritmoGenetico.condicaoDeTerminioAtendida(geracao, numeroMaximoGeracoes) === false) {
+  populacao = algoritmoGenetico.cruzemento(populacao)
+  populacao = algoritmoGenetico.mutacao(populacao)
+  algoritmoGenetico.avaliacao(labirinto, populacao)
+  geracao++;
 }
 
-let cromossomo : Cromossomo = populacao.getCromossoMaisApto(0);
+let cromossomo: Cromossomo = populacao.getCromossoMaisApto(0);
 
 
 const grafo = new Grafo()
 const caminhoGenetico = `${algoritmoGenetico.rota.join('\n')}`;
-const caminhoAestrela = `${procuraAestrela(grafo, [0,0], [11,11]).join('\n')}`
+const caminhoAestrela = `${procuraAestrela(grafo, [0, 0], [11, 11]).join('\n')}`
+
+console.log('Caminho Genetico')
+console.log(caminhoGenetico)
+
+console.log('\n')
+
+console.log('Caminho A*')
+console.log(caminhoAestrela)
 
 fs.writeFile('genetico.txt', caminhoGenetico, function (err) {
   if (err) return console.log(err);
